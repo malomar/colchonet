7ef05b9722b5002248c772c55ed707ee30b62d19
Malomar Seminotti <malomar@wonder.com.br>
2018-02-14 14:04:07 -0200

    Capítulo 7

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  resources :rooms
-  resources :users
+  # Idiomas suportados pela aplicação
+  LOCALES = /en|pt\-BR/
+  scope ":locale", locale: LOCALES do
+    resources :rooms
+    resources :users
+  end
 
+  # Para a página inicial respeitar o idioma
+  get '/:locale' => 'home#index', locale: LOCALES
+  
   root 'home#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
